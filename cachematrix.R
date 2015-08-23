c7d4bbaec1ada93a9144a0bf75116f9f8b63061b
datakatsudon <datakatsudon@users.noreply.github.com>
2015-08-23 11:09:41 -0700

    added cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    ##sets value of matrix
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  ##gets the value of the matrix
+  setinverse <- function(solve) m <<- mean
+  ##sets value of inverse
+  getinverse <- function() m
+  ##gets value of inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
